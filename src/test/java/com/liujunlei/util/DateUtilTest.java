a98f6e0bfba340aee4c63d12f48f7238286f6fc9
augBth <2492860917@qq.com>
2020-01-03 14:11:33 +0800

    工具类

---
+++ src/test/java/com/liujunlei/util/DateUtilTest.java
@@ -0,0 +1,37 @@
+package com.liujunlei.util;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class DateUtilTest {
+
+	@Test
+	public void testInitMonth() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date date = DateUtil.initMonth(new Date());
+		System.out.println(df.format(date));
+	}
+
+	@Test
+	public void testEndMonth() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");	
+		Date date = DateUtil.endMonth(new Date());
+		System.out.println(df.format(date));
+	}
+
+
+	@Test
+	public void testRandomDate() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		for (int i = 0; i < 1000; i++) {
+			Date date = DateUtil.randomDate(new Date(0), new Date());
+			System.out.println(df.format(date));
+
+		}
+	}
+
+}
