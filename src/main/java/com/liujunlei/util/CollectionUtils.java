7c2ff5093eaaa0c206d59da3d0c8c07372ce0442
augBth <2492860917@qq.com>
2020-02-11 17:41:39 +0800

    工具类 工具类

---
+++ src/main/java/com/liujunlei/util/CollectionUtils.java
@@ -0,0 +1,14 @@
+package com.liujunlei.util;
+
+import java.util.Collection;
+
+public class CollectionUtils {
+
+	
+	public static boolean isEmpty(Collection<?> collection) {
+		
+		return collection == null || collection.size()==0;
+		
+	}
+	
+}
---
+++ src/test/java/com/liujunlei/util/CollectionUtilsTest.java
@@ -0,0 +1,27 @@
+package com.liujunlei.util;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+
+
+public class CollectionUtilsTest {
+
+	@Test
+	public void testIsEmpty() {
+		
+		/*
+		 * ArrayList<String> list = new ArrayList<String>(); list.add("a"); boolean b =
+		 * CollectionUtils.isEmpty(list);
+		 */
+		LinkedList list2 = new LinkedList();
+		ArrayList<String> list = new ArrayList<String>();
+		boolean b2 = CollectionUtils.isEmpty(list);
+		System.out.println(b2);
+		
+	}
+
+
+}
